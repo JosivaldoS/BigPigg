72be2ad3f9fa8a400ba9d2c735a93d6930b43763
Josivaldo Soares <77576339+JosivaldoS@users.noreply.github.com>
2024-08-22 12:02:57 -0300

    Funções funcionando
    Consegui fazer com que as senhas fossem verificadas para que elas tenham:
    Ao menos um número;
    Ao menos uma letra maiúscula;
    Sejam maior que 8 números;

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import Input from "../Form/Input/Input";
 import Select from "../Form/Select/Select";
 import SubmitButton from "../Form/SubmitButton/SubmitButton";
 
 export default function ProjectForm({btnTxt}){
+    const [categories, setCategories] = useState([])
+
+    fetch('http://localhost:5000/categories', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+
     return (
         <form>
             <Input label={"Nome do projeto"} placeholder={"Nome do projeto"} type={"text"}/>
